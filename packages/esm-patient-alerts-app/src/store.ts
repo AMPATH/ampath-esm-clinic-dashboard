9f72e5e62f797a7790c9c4c85817b3bc96d1809a
Dennis Kigen <kigen.work@gmail.com>
2021-10-19 15:02:02 +0300

    Improvements to patient alerts
    This PR adds several improvements to the patient alerts feature that include:
    
    - Using SWR hooks for fetching alerts data. This provides all the SWR goodness out of the box, allowing us to leverage
    features such as caching, automatic revalidation and conditional data fetching. The latter is especially important in our case as now
    we don't need to use convoluted logic to keep track of patient ids. Each time a new patient chart is loaded, SWR fetches alerts data and clears the cache.
    - Removing reducer-based view state management logic from the PatientAlertsComponent. The logic is now more readable and easier to understand.
    - Simplification of the logic that keeps track of whether new alerts have been read or not.

--- packages/esm-patient-alerts-app/src/store.ts
+++ packages/esm-patient-alerts-app/src/store.ts
@@ -1,26 +1,24 @@
 import { Store } from 'unistore';
 import { createGlobalStore } from '@openmrs/esm-framework';
-import { Reminder } from './patient-alerts.resource';
 
 export interface PatientAlertsStore {
-  alerts: Array<Reminder>;
   hasUnreadAlerts: boolean;
+  hasViewedAlerts: boolean;
   patientId: string | null;
 }
 
 export const patientAlertsStore: Store<PatientAlertsStore> = createGlobalStore('patient-alerts', {
-  alerts: null,
   hasUnreadAlerts: false,
+  hasViewedAlerts: false,
   patientId: null,
 });
-
-export const setAlerts = patientAlertsStore.action((state, alerts: Array<Reminder>, patientId: string) => ({
-  ...state,
-  alerts: alerts,
-  patientId: patientId,
-}));
 
 export const setHasUnreadAlerts = patientAlertsStore.action((state, value: boolean) => ({
   ...state,
   hasUnreadAlerts: value,
+}));
+
+export const setHasViewedAlerts = patientAlertsStore.action((state, value: boolean) => ({
+  ...state,
+  hasViewedAlerts: value,
 }));
